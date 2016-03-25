60ceb3e02ec2619aeb58f822e13248faa616889f
StephaneMangin <stephane.mangin@freesbee.fr>
2016-03-25 16:16:09 +0100

    Clean and refactor

--- modeling_workbench/org.irisa.diverse.videogen.k3dsa/xtend-gen/videoGen/aspects/VideoAspect.java
+++ modeling_workbench/org.irisa.diverse.videogen.k3dsa/xtend-gen/videoGen/aspects/VideoAspect.java
@@ -1,23 +1,14 @@
 package videoGen.aspects;
 
 import fr.inria.diverse.k3.al.annotationprocessor.Aspect;
-import org.eclipse.xtext.xbase.lib.InputOutput;
-import videoGen.Video;
-import videoGen.aspects.VideoAspectVideoAspectProperties;
 
-@Aspect(className = Video.class)
-@SuppressWarnings("all")
+/* @Aspect(className = /* name is null */) */@SuppressWarnings("all")
 public class VideoAspect {
-  public static void select(final Video _self) {
-    videoGen.aspects.VideoAspectVideoAspectProperties _self_ = videoGen.aspects.VideoAspectVideoAspectContext.getSelf(_self);
-    _privk3_select(_self_, _self);
-  }
-  
-  protected static void _privk3_select(final VideoAspectVideoAspectProperties _self_, final Video _self) {
-    _self.setSelected(Boolean.valueOf(true));
-    String _name = _self.getName();
-    String _plus = ("##### Video \'" + _name);
-    String _plus_1 = (_plus + "\' has been selected.");
-    InputOutput.<String>println(_plus_1);
+  public void select() {
+    throw new Error("Unresolved compilation problems:"
+      + "\nThe method or field _self is undefined for the type VideoAspect"
+      + "\nThe method or field _self is undefined for the type VideoAspect"
+      + "\nselected cannot be resolved"
+      + "\nname cannot be resolved");
   }
 }
--- modeling_workbench/org.irisa.diverse.videogen.k3dsa/xtend-gen/videoGen/aspects/VideoGenAspect.java
+++ modeling_workbench/org.irisa.diverse.videogen.k3dsa/xtend-gen/videoGen/aspects/VideoGenAspect.java
@@ -3,28 +3,20 @@ package videoGen.aspects;
 import fr.inria.diverse.k3.al.annotationprocessor.Aspect;
 import fr.inria.diverse.k3.al.annotationprocessor.Main;
 import fr.inria.diverse.k3.al.annotationprocessor.Step;
-import java.util.HashMap;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.xtext.xbase.lib.InputOutput;
-import org.irisa.diverse.playlist.PlayList.PlayList;
-import org.irisa.diverse.playlist.util.PlayListTransform;
-import org.irisa.diverse.videogen.transformations.helpers.VideoGenTransform;
-import videoGen.Alternatives;
-import videoGen.Mandatory;
-import videoGen.Optional;
-import videoGen.Sequence;
-import videoGen.Video;
-import videoGen.VideoGen;
-import videoGen.aspects.SequenceAspect;
-import videoGen.aspects.VideoGenAspectVideoGenAspectProperties;
 
-@Aspect(className = VideoGen.class)
-@SuppressWarnings("all")
+/* @Aspect(className = /* name is null */) */@SuppressWarnings("all")
 public class VideoGenAspect {
   @Main
-  public static void initialize(final VideoGen _self) {
-    videoGen.aspects.VideoGenAspectVideoGenAspectProperties _self_ = videoGen.aspects.VideoGenAspectVideoGenAspectContext.getSelf(_self);
-    _privk3_initialize(_self_, _self);
+  public void initialize() {
+    throw new Error("Unresolved compilation problems:"
+      + "\nThe method or field _self is undefined for the type VideoGenAspect"
+      + "\nThe method or field _self is undefined for the type VideoGenAspect"
+      + "\nThe method or field _self is undefined for the type VideoGenAspect"
+      + "\nname cannot be resolved"
+      + "\nsequences cannot be resolved"
+      + "\nget cannot be resolved"
+      + "\nprocess cannot be resolved"
+      + "\ncompute cannot be resolved");
   }
   
   /**
@@ -33,75 +25,35 @@ public class VideoGenAspect {
    * @see : ffmpeg
    */
   @Step
-  public static void compute(final VideoGen _self) {
-    fr.inria.diverse.k3.al.annotationprocessor.stepmanager.StepCommand command = new fr.inria.diverse.k3.al.annotationprocessor.stepmanager.StepCommand() {
-    	@Override
-    	public void execute() {
-    		videoGen.aspects.VideoGenAspectVideoGenAspectProperties _self_ = videoGen.aspects.VideoGenAspectVideoGenAspectContext.getSelf(_self);
-    		_privk3_compute(_self_, _self);
-    	}
-    };
-    fr.inria.diverse.k3.al.annotationprocessor.stepmanager.IStepManager manager = fr.inria.diverse.k3.al.annotationprocessor.stepmanager.StepManagerRegistry.getInstance().findStepManager(_self);
-    if (manager != null) {
-    	manager.executeStep(_self,command,"VideoGen","compute");
-    } else {
-    	command.execute();
-    }
-  }
-  
-  protected static void _privk3_initialize(final VideoGenAspectVideoGenAspectProperties _self_, final VideoGen _self) {
-    String _name = _self.getName();
-    String _plus = ("##### VideoGen \'" + _name);
-    String _plus_1 = (_plus + "\' has been initialized.");
-    InputOutput.<String>println(_plus_1);
-    EList<Sequence> _sequences = _self.getSequences();
-    final Sequence sequence = _sequences.get(0);
-    SequenceAspect.process(sequence);
-    VideoGenAspect.compute(_self);
-  }
-  
-  protected static void _privk3_compute(final VideoGenAspectVideoGenAspectProperties _self_, final VideoGen _self) {
-    final HashMap<String, Boolean> videos = new HashMap<String, Boolean>();
-    String _name = _self.getName();
-    String _plus = ("##### VideoGen \'" + _name);
-    String _plus_1 = (_plus + "\' start computation.");
-    InputOutput.<String>println(_plus_1);
-    EList<Sequence> _sequences = _self.getSequences();
-    for (final Sequence sequence : _sequences) {
-      if ((sequence instanceof Mandatory)) {
-        Video _video = ((Mandatory)sequence).getVideo();
-        String _name_1 = _video.getName();
-        Video _video_1 = ((Mandatory)sequence).getVideo();
-        Boolean _selected = _video_1.getSelected();
-        videos.put(_name_1, _selected);
-      } else {
-        if ((sequence instanceof Optional)) {
-          Video _video_2 = ((Optional)sequence).getVideo();
-          String _name_2 = _video_2.getName();
-          Video _video_3 = ((Optional)sequence).getVideo();
-          Boolean _selected_1 = _video_3.getSelected();
-          videos.put(_name_2, _selected_1);
-        } else {
-          if ((sequence instanceof Alternatives)) {
-            EList<Optional> _options = ((Alternatives)sequence).getOptions();
-            for (final Optional option : _options) {
-              Video _video_4 = option.getVideo();
-              Boolean _selected_2 = _video_4.getSelected();
-              if ((_selected_2).booleanValue()) {
-                Video _video_5 = option.getVideo();
-                String _name_3 = _video_5.getName();
-                Video _video_6 = option.getVideo();
-                Boolean _selected_3 = _video_6.getSelected();
-                videos.put(_name_3, _selected_3);
-              }
-            }
-          }
-        }
-      }
-    }
-    InputOutput.<String>println("##### Videos computation result in playlist format : ");
-    final PlayList playlist = VideoGenTransform.toCustomPlayList(_self, Boolean.valueOf(true), videos);
-    String _m3U = PlayListTransform.toM3U(playlist, Boolean.valueOf(true));
-    InputOutput.<String>println(_m3U);
+  public void compute() {
+    throw new Error("Unresolved compilation problems:"
+      + "\nSequence cannot be resolved to a type."
+      + "\nMandatory cannot be resolved to a type."
+      + "\nOptional cannot be resolved to a type."
+      + "\nAlternatives cannot be resolved to a type."
+      + "\nOptional cannot be resolved to a type."
+      + "\nThe method or field _self is undefined for the type VideoGenAspect"
+      + "\nThe method or field _self is undefined for the type VideoGenAspect"
+      + "\nThe method or field _self is undefined for the type VideoGenAspect"
+      + "\nType mismatch: cannot convert from HashMap<Object, Object> to Map<String, Boolean>"
+      + "\nUnreachable code: The if condition can never match. It is already handled by a previous condition."
+      + "\nUnreachable code: The if condition can never match. It is already handled by a previous condition."
+      + "\nname cannot be resolved"
+      + "\nsequences cannot be resolved"
+      + "\nvideo cannot be resolved"
+      + "\nname cannot be resolved"
+      + "\nvideo cannot be resolved"
+      + "\nselected cannot be resolved"
+      + "\nvideo cannot be resolved"
+      + "\nname cannot be resolved"
+      + "\nvideo cannot be resolved"
+      + "\nselected cannot be resolved"
+      + "\noptions cannot be resolved"
+      + "\nvideo cannot be resolved"
+      + "\nselected cannot be resolved"
+      + "\nvideo cannot be resolved"
+      + "\nname cannot be resolved"
+      + "\nvideo cannot be resolved"
+      + "\nselected cannot be resolved");
   }
 }
