7160d6a43117ff5a6843d80145fc5e3d4b7da9b4
StephaneMangin <stephane.mangin@freesbee.fr>
2016-04-05 11:11:15 +0200

    Complete code cleaning

--- language_Workbench/org.irisa.diverse.videogen.k3dsa/xtend-gen/org/irisa/diverse/videogen/videoGen/aspects/SequenceAspectSequenceAspectProperties.java
+++
@@ -1,8 +0,0 @@
-package org.irisa.diverse.videogen.videoGen.aspects;
-
-@SuppressWarnings("all")
-public class SequenceAspectSequenceAspectProperties {
-  public Boolean done = Boolean.valueOf(false);
-  
-  public Boolean callNextSequence = Boolean.valueOf(true);
-}
--- language_Workbench/org.irisa.diverse.videogen.k3dsa/xtend-gen/org/irisa/diverse/videogen/videoGen/aspects/VideoAspectVideoAspectProperties.java
+++
@@ -1,6 +0,0 @@
-package org.irisa.diverse.videogen.videoGen.aspects;
-
-@SuppressWarnings("all")
-public class VideoAspectVideoAspectProperties {
-  public Boolean selected = Boolean.valueOf(false);
-}
