10c133fde5b352421d4a6386385dad513a52ed79
StéphaneMangin <stephane.mangin@freesbee.fr>
2016-04-27 19:15:57 +0200

    End of the refactoring

--- language_Workbench/org.irisa.diverse.videogen.xdsml.debug.design/src/org/irisa/diverse/videogen/xdsml/debug/design/services/VideogenlDebugServices.java
+++ language_Workbench/org.irisa.diverse.videogen.xdsml.debug.design/src/org/irisa/diverse/videogen/xdsml/debug/design/services/VideogenlDebugServices.java
@@ -3,9 +3,7 @@ package org.irisa.diverse.videogen.xdsml.debug.design.services;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.emf.ecore.EObject;
 import org.gemoc.executionframework.extensions.sirius.services.AbstractGemocDebuggerServices;
-import org.irisa.diverse.videogen.videoGen.Transition;
 
 public class VideogenlDebugServices extends AbstractGemocDebuggerServices {
 
@@ -18,25 +16,4 @@ public class VideogenlDebugServices extends AbstractGemocDebuggerServices {
 		return res;
 	}
 
-	@Override
-	public String getModelIdentifier() {
-		return org.gemoc.execution.sequential.javaengine.ui.Activator.PLUGIN_ID+".debugModel";
-	}
-	
-	/**
-	 * Tells if the given {@link EObject instruction} is a currently debugged
-	 * instruction. A debugged instruction in this context is an instruction a
-	 * debug target is suspended on.
-	 * 
-	 * @param instruction
-	 *            the {@link EObject instruction}
-	 * @return <code>true</code> if the given {@link EObject instruction} is a
-	 *         currently selected sequence, <code>false</code> otherwise
-	 */
-	public boolean isSelected(EObject instruction) {
-		if (instruction instanceof Transition) {
-			return ((Transition) instruction).getSelected();
-		}
-		return false;
-	}
 }
